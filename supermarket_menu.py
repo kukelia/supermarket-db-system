0a2207f0b475f6cbaa16c046f44c129407425a96
tomas <zubik.tomas@gmail.com>
2022-12-19 20:57:45 -0300

    v1.0

--- supermarket_menu.py
+++ supermarket_menu.py
@@ -32,38 +32,77 @@ def ver():
 
 def agregar_producto():
     id = input("ingrese id: ")
-    nombre = input("ingrese nombre producto: ")
-    precio = input("ingrese precio: ")
-    cat = input("ingrese categoria: ")
-    cursor.execute( f"INSERT INTO productos VALUES ({id}, '{nombre}', {precio}, '{cat}');")
+    cursor.execute(f"SELECT * FROM productos WHERE id_producto = {id};")
+    if cursor.rowcount == 0:
+        nombre = input("ingrese nombre producto: ")
+        precio = input("ingrese precio: ")
+        cat = input("ingrese categoria: ")
+        cursor.execute( f"INSERT INTO productos VALUES ({id}, '{nombre}', {precio}, '{cat}');")
+    else:
+        print("el producto ya existe")
 
 
 def realizar_compra():
-    dni = input("ingrese dni cliente: ")
-    cursor.execute(f"SELECT * FROM clientes WHERE id == {dni};")
-    if cursor.rowcount == 0:
-        print("el cliente no existe, debera ser agregado")
-        agregar_cliente(dni)
-    cursor.execute("BEGIN;")
-    cursor.execute(f"""INSERT INTO compras (dni_cliente, dni_empleado)
-                    VALUES {dni}, {dni_empleado} RETURNING id_compra""")
-    for id_producto in carrito:
-        cursor.execute(f"""INSERT INTO productos_comprados (id_compra, id_producto)")
-    VALUES (id_compra, {id_producto}); """)
-    cursor.execute(f"""SELECT productos_comprados.id_producto, productos.precio
-                    FROM productos_comprados
-                    INNER JOIN productos ON productos_comprados.id_producto = productos.id_producto
-                    GROUP BY productos_comprados.id_producto;
-                    
-    """)
+    # try:
+        global carrito
+        dni = input("ingrese dni cliente: ")
+        cursor.execute(f"SELECT * FROM clientes WHERE dni = {dni};")
+        if cursor.rowcount == 0:
+            print("el cliente no existe, debera ser agregado")
+            agregar_cliente(dni)
+        
+        cursor.execute("BEGIN;")
+        cursor.execute(f"""INSERT INTO compras (dni_cliente, dni_empleado)
+                        VALUES ({dni}, {dni_empleado}) RETURNING *""")
+        id_compra = cursor.fetchone()[0]
+        for id_producto in carrito:
+            cursor.execute(f"""
+                                INSERT INTO productos_comprados (id_compra, id_producto)
+                                VALUES ({id_compra}, {id_producto}) RETURNING producto_vendido ;""")
+            id_producto_vendido = cursor.fetchone()[0] 
+            print(f"id del producto vendido es {id_producto_vendido}")      
+            cursor.execute(f"""       
+                                with precio_actual as
+                                (SELECT precio FROM productos WHERE id_producto={id_producto})
+                                UPDATE productos_comprados
+                                SET precio_compra = precio_actual.precio FROM precio_actual
+                                WHERE producto_vendido = {id_producto_vendido}
+                                """)
 
-    cursor.execute("COMMIT;")
+            os.system(f"""echo "LOG
+                INSERT INTO productos_comprados (id_compra, id_producto)
+                VALUES ({id_compra}, {id_producto}) RETURNING producto_vendido ;
+
+                with precio_actual as
+                (SELECT precio FROM productos WHERE id_producto={id_producto})
+                UPDATE productos_comprados
+                SET precio_compra = precio_actual.precio FROM precio_actual
+                WHERE producto_vendido = {id_producto_vendido}" >> logs/log.txt
+                """)  
+
+        cursor.execute(f"""
+                    SELECT sum FROM(
+                        SELECT productos_comprados.id_compra, SUM(productos.precio)
+                        FROM productos_comprados
+                        INNER JOIN productos ON productos_comprados.id_producto = productos.id_producto
+                        GROUP BY productos_comprados.id_compra
+            ) as result_table
+        where result_table.id_compra={id_compra}
+        """)
+        precio_total = cursor.fetchone()[0]
+        cursor.execute(f"UPDATE compras SET precio_total={precio_total} WHERE id_compra=id_compra")
+
+        cursor.execute("COMMIT;")
+        carrito= []
+    # except:
+    #     print("ha ocurrido un error")
+    #     cursor.execute("ROLLBACK;")
     
-    
+
 
 def agregar_carrito():
     id = input("ingrese id: ")
-    cursor.execute(f"SELECT * FROM productos WHERE id == {id};")
+    cursor.execute(f"SELECT * FROM productos WHERE id_producto = {id};")
     if cursor.rowcount == 0:
         print("no existe ese producto")
         return
@@ -87,24 +126,48 @@ def agregar_cliente(dni):
     edad = input("ingrese edad: ")
     cursor.execute( f"INSERT INTO clientes VALUES ({dni}, '{nombre}', '{ape}', {edad});")
 
+
+def agregar_empleado(dni):
+    nombre = input("ingrese nombre del empleado: ")
+    ape = input("ingrese apellido del empleado: ")
+    edad = input("ingrese edad: ")
+    cursor.execute( f"""INSERT INTO empleados (dni,nombre,apellido,edad)
+                        VALUES ({dni}, '{nombre}', '{ape}', {edad});""")
+
+def ver_carrito():
+    print(carrito)
+
+
+
 carrito = []
 x = 0
 dni_empleado=0
 
+#----MENU-----
+
 while True:
-    if x == 1:
+    if dni_empleado != 0:
         input("presione enter para continuar")
     else:
-        x = 1
+        dni_empleado = input("ingrese su dni empleado: ")
+        cursor.execute(f"SELECT * FROM empleados WHERE dni = {dni_empleado};")
+        if cursor.rowcount == 0:
+            print("no existe ese empleado, se creara su perfil...")
+            agregar_empleado(dni_empleado)
+        else:
+            print("Bienvenido al sistema!")
+
     os.system("clear")
     print("--------------------------------------------")
     print("bienvenido al supermercado! que desea hacer?")
     operation = int(input(""" 
+    0- agregar producto al carrito
     1- realizar compra
     2- agregar producto
     3- eliminar producto
     4- ver datos
-    5-salir
+    5- ver carrito
+    6- salir
     
     -> """))
 
@@ -120,6 +183,8 @@ while True:
         case 4:
             ver()
         case 5:
+            ver_carrito()
+        case 6:
             conn.close()
             print("conexion cerrada")
             break
